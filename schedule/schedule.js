00a94f72807364145f2d84a16ca4cd9d99eb8027
dtd08 <ddwt0826@naver.com>
2025-10-17 11:09:19 +0900

    강의일정 세부사항 구조 및 스타일 조정

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -4,19 +4,19 @@ const contentBox = document.getElementById("content-box");
 const detailBtn = document.getElementById("detail-btn");
 const btnBox = document.getElementById("btn-box");
 
+let btnWidth = 40;
 let contentBoxWidth = 350;
-
 
 // func
 function toggleDetailBox() {
     if (detailBtn.classList[0] == "close") {  // 닫힌 상태에서 열기
-        scheduleBox.style.right = 0;  // 상세 페이지 숨기기
+        scheduleBox.style.width = contentBoxWidth + "px";
 
         detailBtn.classList.remove("close");
         detailBtn.classList.add("open");
     } 
     else if (detailBtn.classList[0] == "open") {  // 열린 상태에서 닫기
-        scheduleBox.style.right = contentBoxWidth * -1 + "px";
+        scheduleBox.style.width = btnWidth + "px";  // 상세 페이지 숨기기
 
         detailBtn.classList.remove("open");
         detailBtn.classList.add("close");
